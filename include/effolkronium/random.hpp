bbc301c43dfe2d8c432cc71a58a8846005016d26
Illia <ilqvya@icloud.com>
2026-01-18 15:33:56 -0800

    Update copyright year random.hpp

--- include/effolkronium/random.hpp
+++ include/effolkronium/random.hpp
@@ -7,7 +7,7 @@ ______  ___   _   _______ ________  __
 \_| \_\_| |_/\_| \_/___/  \___/\_|  |_/ https://github.com/ilqvya/random
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
-Copyright (c) 2017-2025 ilqvya (Illia Polishchuk)
+Copyright (c) 2017-2026 ilqvya (Illia Polishchuk)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files( the "Software" ), to deal
